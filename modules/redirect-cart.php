6cae8c1f18f0696b4d0d16a2c4443bbff01df64f
Surbma <peter@surbma.hu>
2022-09-30 22:46:56 +0200

    Fix redirection for “Redirect Cart page to Checkout page” module, when cart is empty.

--- modules/redirect-cart.php
+++ modules/redirect-cart.php
@@ -2,7 +2,11 @@
 
 add_action( 'template_redirect', function() {
 	if ( is_cart() ) {
-		wp_safe_redirect( wc_get_checkout_url() );
+		if ( WC()->cart->get_cart_contents_count() == 0 ) {
+			wp_safe_redirect( wc_get_page_permalink( 'shop' ) );
+		} else {
+			wp_safe_redirect( wc_get_checkout_url() );
+		}
 		exit;
 	}
 } );
